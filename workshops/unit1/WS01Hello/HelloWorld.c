898be911b2bfe5aebb0f39695f1c3e5b7accd34f
CatherineLuje <katyluje@gmail.com>
2021-01-08 13:28:08 -0500

    WS01 Hello  World in three programing languages

---
+++ workshops/unit1/WS01Hello/HelloWorld.c
@@ -0,0 +1,9 @@
+#incluide <stdio.h>
+int main (){
+   printf("Hello World \n from Catherine Luje!=) \n  ");
+   Printf("\ The grade is %d \n", grade);
+  
+   return 0;
+  }
+  
+  
---
+++ workshops/unit1/WS01Hello/HelloWorld.py
@@ -0,0 +1,11 @@
+print("Hello World fron Catherine Luje" \n ESPE \n GEO" )
+
+addend1= input ('Enter the first number -->')
+addend2= input('Enter the second number -->')
+
+sum= float(addend1)+ float(addend2)
+
+print('The sum of {o} and {1} is {2}'.format(addend1, addend2, sum))
+
+
+print('The sum of % if' %(float(imput('Enter first number:'))+float(input('Enter second number:'))))
